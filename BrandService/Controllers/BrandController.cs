72a4f526aea3b24665d9996fa49498db1bc82361
Pepijn <pepijn.mus@gmail.com>
2022-04-05 14:24:56 +0200

    Update Controlername

--- BrandService/Controllers/BrandController.cs
+++ BrandService/Controllers/BrandController.cs
@@ -9,6 +9,7 @@ namespace BrandService.Controllers
     public class BrandController : Controller
     {
         private string connectionString = "Server=studmysql01.fhict.local;Uid=dbi458416;Database=dbi458416;Pwd=1234";
+        //private string connectionString = "server=localhost;user=root;database=pimwoc;port=3306;password='';SslMode=none";
 
         private string query;
         MySqlConnection connection; 
@@ -18,34 +19,54 @@ namespace BrandService.Controllers
             query = "";
 
         }
-        [HttpGet]
-        public string Get(string name)
+        [HttpGet("Brand")]
+        public string Brand(string name)
+        {
+            Brand brand = new Brand("", "");
+            try
+            {
+                connection.Open();
+                query = $"SELECT brands.Id, brands.Name, country.English FROM `brands` INNER JOIN country ON brands.Country = country.Id WHERE brands.Name = '{name}'";
+                var cmd = new MySqlCommand(query, connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    brand.Id = reader.GetInt32(0);
+                    brand.Name = reader.GetString(1);
+                    brand.Country = reader.GetString(2);
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            connection.Close();
+            return JsonSerializer.Serialize(brand);
+
+        }
+        [HttpGet("AllBrands")]
+        public string Brands()
         {
             List<Brand> brands = new List<Brand>();
             try
             {
                 connection.Open();
-                if(name != null)
-                {
-                    query = $"SELECT brands.Id, brands.Name, country.English FROM `brands` INNER JOIN country ON brands.Country = country.Id WHERE brands.Name = '{name}'";
-                }
-                else
-                {
-                    query = "SELECT brands.Id, brands.Name, country.English FROM `brands` INNER JOIN country ON brands.Country = country.Id;";
 
-                }
+                query = "SELECT brands.Id, brands.Name, country.English FROM `brands` INNER JOIN country ON brands.Country = country.Id;";
                 var cmd = new MySqlCommand(query, connection);
                 var reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    Brand brand = new Brand("","");
+                    Brand brand = new Brand("", "");
                     brand.Id = reader.GetInt32(0);
                     brand.Name = reader.GetString(1);
                     brand.Country = reader.GetString(2);
-                    
+
                     brands.Add(brand);
                 }
-                
+
 
             }
             catch (Exception ex)
