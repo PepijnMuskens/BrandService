2c2510897a4fd21ba33ac592a7169edfb61425da
Pepijn <pepijn.mus@gmail.com>
2022-04-26 14:17:00 +0200

    update whitelist

--- BrandService/Program.cs
+++ BrandService/Program.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,9 +31,67 @@ if (app.Environment.IsDevelopment())
 app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 
+app.UseMiddleware<AdminSafeListMiddleware>("145.93.76.86;51.116.145.38;10.0.0.7;192.168.1.5;::1;145.93.116.244");
+//Gateway ip:       51.116.145.38
+//Brand service ip: 
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
+public class AdminSafeListMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<AdminSafeListMiddleware> _logger;
+    private readonly byte[][] _safelist;
+
+    public AdminSafeListMiddleware(
+        RequestDelegate next,
+        ILogger<AdminSafeListMiddleware> logger,
+        string safelist)
+    {
+        var ips = safelist.Split(';');
+        _safelist = new byte[ips.Length][];
+        for (var i = 0; i < ips.Length; i++)
+        {
+            _safelist[i] = IPAddress.Parse(ips[i]).GetAddressBytes();
+        }
+
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Method == HttpMethod.Get.Method)
+        {
+            var remoteIp = context.Connection.RemoteIpAddress;
+            _logger.LogDebug("Request from Remote IP address: {RemoteIp}", remoteIp);
+
+            var bytes = remoteIp.GetAddressBytes();
+            var badIp = true;
+            foreach (var address in _safelist)
+            {
+                if (address.SequenceEqual(bytes))
+                {
+                    badIp = false;
+                    break;
+                }
+            }
+
+            if (badIp)
+            {
+                _logger.LogWarning(
+                    "Forbidden Request from Remote IP address: {RemoteIp}", remoteIp);
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+
+                return;
+            }
+        }
+
+        await _next.Invoke(context);
+    }
+}
